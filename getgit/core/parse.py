63b276819174b6feb591f8af346752dbb9553fad
Krai53n <greasha46@gmail.com>
2021-01-24 17:54:33 +0300

    (╥_╥)

--- getgit/core/parse.py
+++ getgit/core/parse.py
@@ -18,6 +18,21 @@ def request_html(path):
 
 
 def github_parse_reps(html_doc):
-    '''Catch repositories from Github of user
+    '''Catch repositories from Github page of user.
+    And return list of repositories`s name
     '''
     soup = BeautifulSoup(html_doc, "html.parser")
+    reps = []
+    for tag in soup.find_all("h3"):
+        rep = tag.find("a").string
+        rep = rep.replace("\n", "")
+        rep = rep.replace(" ", "")
+        reps.append(rep)
+    return reps
+
+
+if __name__ == "__main__":
+    PATH = "https://github.com/Krai53n?tab=repositories"
+    html_doc = request_html(PATH)
+    reps = github_parse_reps(html_doc)
+    print(reps)
