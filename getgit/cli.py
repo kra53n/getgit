6267d8e18dbaee0a9aa250f0e74555ef79231173
Krai53n <greasha46@gmail.com>
2021-01-30 20:35:12 +0300

    (＃￣0￣)

--- getgit/cli.py
+++ getgit/cli.py
@@ -1,1 +1,17 @@
 from simple_term_menu import TerminalMenu
+
+
+def introduce_program():
+    txt = """
+    Welocme to getgit! I Hope that you will enjoy this program!
+    From people to people!
+    """
+    print(txt)
+
+def ask_git_version_service():
+    txt = "Choose git version that you use:\n"
+    gits = ("github", "gitlab")
+
+
+if __name__ == '__main__':
+    pass
