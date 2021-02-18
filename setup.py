adc6c6019488dfb0e15259300bc707785fd86b45
Krai53n <greasha46@gmail.com>
2021-02-18 07:37:02 +0300

    Add OS dependency

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
     install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Operating System :: POSIX :: Linux",
     ],
     entry_points={
         "console_scripts": 
