2398e5413727e57e59749e567bc0bd68de0dcb44
Krai53n <greasha46@gmail.com>
2021-02-28 15:50:12 +0300

    Fixed work on Windows

--- getgit/config/wwyaml.py
+++ getgit/config/wwyaml.py
@@ -6,7 +6,7 @@ from yaml import dump
 
 from sys import exit
 from sys import platform
-from os import mkdir
+from os import makedirs
 
 
 from pathlib import Path
@@ -14,16 +14,13 @@ path_conf_file = str(Path.home())
 if platform == "linux":
     path_conf_file += "/.config/getgit/config.yaml"
 if platform == "win32":
-    # TODO: need here r or not
     path_conf_file += r"\.config\getgit\config.yaml"
 
 
 def create_file(path=path_conf_file):
     """Create file in define path
     """
-    # TODO: check working of mkdir
-    # make simulation with path and actions that located under
-    mkdir(path[:-12])
+    makedirs(path[:-12])
     with open(path, "w") as f:
         f.write("service:\nnickname:")
 
