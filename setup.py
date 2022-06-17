ca30cc604363de01e23e07a27f7e3a24d7ba84c8
Krai53n <greasha46@gmail.com>
2022-06-17 22:43:54 +0300

    fix setup

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: POSIX :: Linux",
     ],
+    packages=['getgit'],
     package_data={
         '': [
             'config\*.yaml',
