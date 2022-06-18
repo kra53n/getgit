059a733cceaf16bf4f451a8d4ff98e5e910e7900
Krai53n <greasha46@gmail.com>
2022-06-18 21:36:04 +0300

    delete ununiversal clone functions

--- getgit/clone.py
+++ getgit/clone.py
@@ -2,16 +2,6 @@ from os import system
 from .parse import get_config_data
 
 
-def clone_github_rep(nickname, rep_name):
-    print("git clone git@github.com:{}/{}.git".format(nickname, rep_name))
-    system("git clone git@github.com:{}/{}.git".format(nickname, rep_name))
-
-
-def clone_notabug_rep(nickname, rep_name):
-    print("git clone https://notabug.org/{}/{}".format(nickname, rep_name))
-    system("git clone https://notabug.org/{}/{}".format(nickname, rep_name))
-
-
 def clone_rep(service: str, nickname: str, rep_name: str):
     message = get_config_data()[service]['rep'].replace('nickname', nickname).replace('rep_name', rep_name)
     print(message)
