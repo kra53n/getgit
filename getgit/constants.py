6c608736550b7adf71054095d81bdb351040cc48
kra53n <59062496+kra53n@users.noreply.github.com>
2022-06-27 15:28:44 +0300

    Update constants.py

--- getgit/constants.py
+++ getgit/constants.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 PROG_NAME = 'getgit'
-PROG_VERS = '0.1.1'
+PROG_VERS = '0.1.2'
 
 CONFIG_DIR = Path.home() / '.config/getgit'
 CONFIG_PARSE_PATH = Path(__file__).parent / 'config/parse.yaml'
